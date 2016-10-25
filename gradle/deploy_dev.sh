989a97345429a4422e7184f0138f5fb4e1710c82
chenrongrong <rongrong.chen@alcatel-sbell.com.cn>
2016-10-25 15:18:48 +0800

    21

--- gradle/deploy_dev.sh
+++ gradle/deploy_dev.sh
@@ -1,15 +1,15 @@
-cd /home/smas/apache-tomcat-8.0.0-RC5/bin
+cd /home/app/apache-tomcat-8.0.0-RC5/bin
 ./kill.sh
 echo 'TOMCAT KILLED'
 
 rm -Rf ../logs/*
 echo 'LOG CLEANED'
 
-rm -Rf ../webapps/idas*
+rm -Rf ../webapps/ROOT*
 
 echo 'OLD VERSION REMOVED'
 
-cp $SMAS_BUILD/smas_build_dev/idas.war /home/smas/apache-tomcat-8.0.0-RC5/webapps
+cp ./message6_build/ROOT.war /home/app/apache-tomcat-8.0.0-RC5/webapps
 
 echo 'NEW VERSION INSTALLED'
 echo 'STARTING...'
